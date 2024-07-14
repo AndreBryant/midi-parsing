147d53aff32b9c7771861fb38e9f6d8e506273fe
embryzon <95128949+embryzon@users.noreply.github.com>
2024-07-14 13:28:59 +0800

    add file reading

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { MIDI } from "./midi.js";
 
-new MIDI("asjdnajdc");
-console.log("hello world");
+const midiData = new MIDI();
+midiData.parseFile("Pretender.mid");
--- src/midi.ts
+++ src/midi.ts
@@ -1,8 +1,23 @@
 import * as fs from "fs";
 import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class MIDI {
-  constructor(path: string) {
-    console.log(path);
+  constructor() {}
+
+  parseFile(filePath: string) {
+    const absolutePath = path.resolve(__dirname, "../assets/midi", filePath);
+
+    fs.readFile(absolutePath, (error, fileContents) => {
+      if (error) {
+        console.error("Error Reading FIle: ", error);
+        return;
+      }
+      // console.log(fileContents);
+      console.log(fileContents.toString("hex"));
+    });
   }
 }
