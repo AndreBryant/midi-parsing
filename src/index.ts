ea936e5b80a72d74c85f54d6fdb00e80bc95580a
embryzon <95128949+embryzon@users.noreply.github.com>
2024-07-14 23:51:45 +0800

    add reading each track haha

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { MIDI } from "./midi.js";
 
 (async () => {
   const midi = new MIDI();
-  await midi.readMidiData("Pretender.mid");
-  //   await midi.parseFile("Pretender.mid");
+  midi.readMidiData("Pretender.mid");
 
-  console.log(midi.getMidiData());
   console.log(midi.getMidiData()?.byteLength);
+  midi.parseMidiData();
 })();
--- src/midi.ts
+++ src/midi.ts
@@ -7,35 +7,76 @@ import type { uint8, uint16, uint32, int8, int16, int32 } from "./types";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// https://ccrma.stanford.edu/~craig/14q/midifile/MidiFileFormat.html#:~:text=Header%20Chunk,-The%20header%20chunk&text=These%20four%20characters%20at%20the,this%20is%20a%20MIDI%20file.&text=length%20of%20the%20header%20chunk,of%20the%20next%20three%20fields).
 export class MIDI {
   private midiBuffer: Buffer | null = null;
   private midiEvents: Buffer | null = null;
   private dataSize: number = 0;
   private position: number = 0;
 
-  constructor() {}
+  constructor();
+  constructor(filePath: string);
+  constructor(filePath?: string) {
+    if (filePath) this.readMidiData(filePath);
+  }
 
-  // DEBUG
   getMidiData() {
     return this.midiBuffer;
   }
 
-  // sets the midi buffer
   readMidiData(filePath: string) {
     const absolutePath = path.resolve(__dirname, "../assets/midi", filePath);
     try {
       this.midiBuffer = fs.readFileSync(absolutePath);
       this.dataSize = this.midiBuffer?.byteLength;
     } catch (error) {
+      console.log("Error: ", error);
       return null;
     }
   }
 
-  // returns JSON of the midi;
-  private parseMidiData() {}
+  parseMidiData() {
+    this.position = 0;
+    const { nFileId, nHeaderLength, nFormat, nTrackChunks, nDivision } =
+      this.readMidiHeader();
 
-  // returns an Object containing an array of events and notes
-  private interpretData() {}
+    for (let i = 0; i < nTrackChunks; i++) {
+      console.log("=====New Track=====");
+
+      let nTrackID = this.readBytes(4);
+      let nTrackLength = this.readBytes(4);
+
+      console.log(nTrackID.toString(16), nTrackLength.toString(16));
+      for (let i = 0; i < nTrackLength; i++) {
+        this.readNextByte();
+      }
+    }
+  }
+
+  interpretMidi() {}
+
+  private readMidiHeader() {
+    this.position = 0;
+
+    let nFileId = this.readBytes(4);
+    let nHeaderLength = this.readBytes(4);
+    let nFormat = this.readBytes(2);
+    let nTrackChunks = this.readBytes(2);
+    let nDivision = this.readBytes(2);
+
+    return { nFileId, nHeaderLength, nFormat, nTrackChunks, nDivision };
+  }
+
+  private readBytes(length: number) {
+    let acc = 0;
+
+    for (let i = 0; i < length; i++) {
+      const byte = this.readNextByte();
+      acc = (acc << 8) | byte;
+    }
+
+    return acc;
+  }
 
   private readNextByte(): number {
     if (this.midiBuffer && this.position < this.dataSize) {
@@ -45,5 +86,22 @@ export class MIDI {
     } else {
       throw new Error("End of buffer reached");
     }
+  }
+
+  private readValue() {
+    let nValue: number[] = [];
+    let current = this.readNextByte();
+
+    nValue.push(current);
+
+    if (current & 0x80) {
+      // keep reading bytes while the MSB is 1
+      // the last byte read should have 0 as its MSB
+      do {
+        current = this.readNextByte();
+        nValue.push(current);
+      } while (current & 0x80);
+    }
+    return nValue;
   }
 }
